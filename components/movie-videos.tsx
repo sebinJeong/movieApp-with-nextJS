e0e969f99c4a98a993d99391d3a4f5525e6797c3
binseJeong <tpqls8791@gmail.com>
2026-04-22 18:47:26 +0900

    add error.tsx in movies

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,13 @@
+import { error } from "node:console";
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`, {
-    cache: "force-cache",
-  });
-  return response.json();
+    await new Promise((resolve)=> (setTimeout(resolve, 3000)));
+    throw new Error("something broke...");
+    //   const response = await fetch(`${API_URL}/${id}/videos`, {
+//     cache: "force-cache",
+//   });
+//   return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
